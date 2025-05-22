b2b8cac58c6963750cd04b871323e6ae1ec8e1e5
ForzaElettromotrice <mingardi.federico@gmail.com>
2025-05-22 13:53:49 +0200

    minor changes

--- include/CUtils/hashmap.h
+++ include/CUtils/hashmap.h
@@ -12,7 +12,7 @@ typedef struct Hashmap Hashmap_t;
 int initHashmap(Hashmap_t *hashmap, size_t dim);
 void freeHashmap(const Hashmap_t *hashmap);
 
-int setByKey(const char *key, const void *val, size_t size, const Hashmap_t *hashmap);
+int setByKey(const char *key, const void *val, size_t size, Hashmap_t *hashmap);
 void *getByKey(const char *key, const Hashmap_t *hashmap);
 
 void removeKey(const char *key, const Hashmap_t *hashmap);
